2ece254857f798d81da1ba455f4e763e86ca03db
imkrysis <elkrysss@gmail.com>
2021-05-01 20:53:11 +0200

    Now camera is activated when the button is clicked, and it lets you to make a photo, then that photo is setted to the imageView

--- www/js/index.js
+++ www/js/index.js
@@ -7,5 +7,29 @@ function onDeviceReady() {
     btnCamera = document.getElementById('btnCamera').onclick = capturePhoto
     imgPhoto = document.getElementById('imgPhoto')
 
+    pictureSource = navigator.camera.PictureSourceType
+    destinationType = navigator.camera.DestinationType
+
+}
+
+function capturePhoto() {
+    
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+
+}
+
+function onPhotoDataSuccess(imageData) {
+
+    alert('Photo saved');
+    
+    imgPhoto.style.display = 'block'
+    imgPhoto.src = 'data:image/jpeg;base64,' + imageData
+}
+
+function onFail(message) {
+
+    alert('Failed cause of: ' + message);
+
 }
 
